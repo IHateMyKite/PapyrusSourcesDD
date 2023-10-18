ae861166f445941ea867f6e7f294391e3a43413b
IHateMyKite <globglogabgalab991@gmail.com>
2023-10-18 17:15:33 +0200

    Added SLIF to build chain

--- BUILD.bat
+++ BUILD.bat
@@ -2,7 +2,7 @@
 
 echo **Running Papyrus Compiler for all script
 
-PapyrusCompiler "../Scripts/Source" -o="../Scripts" -all -i="../Scripts/Source;SRC;SRC_OTHER;SRC_LAL;SRC_ZAZ;SRC_PO3;SRC_DD;SRC_SKYUI;SRC_PAPUTIL;SRC_SXL;SRC_MFG;SRC_RM;SRC_FNIS;SRC_SLA;SRC_XPMSE" -optimize -f="Flags.flg"
+PapyrusCompiler "../Scripts/Source" -o="../Scripts" -all -i="../Scripts/Source;SRC;SRC_SLIF;SRC_OTHER;SRC_LAL;SRC_ZAZ;SRC_PO3;SRC_DD;SRC_SKYUI;SRC_PAPUTIL;SRC_SXL;SRC_MFG;SRC_RM;SRC_FNIS;SRC_SLA;SRC_XPMSE" -optimize -f="Flags.flg"
 
 if NOT ["%errorlevel%"]==["0"] (
     echo **Error compiling one or more scripts
